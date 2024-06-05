a01095def5a4913eb43681541efe98f29fc701d8
nithyarg <nithyaasbsc@gmail.com>
2024-06-05 17:24:34 +0530

    yui

---
+++ url_shortener/extensions.py
@@ -0,0 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy # type: ignore
+
+
+db = SQLAlchemy()
---
+++ url_shortener/routes.py
@@ -0,0 +1,33 @@
+from urllib import request
+from flask import Blueprint,render_template
+
+from .extensions import db
+
+from url_shortener.models import Link
+
+short = Blueprint('short', __name__)
+
+@short.route('/<short_url>')
+def redirect_to_url(short_url):
+    return "hbjn"
+
+@short.route('/')
+def index():
+    return render_template('index.html')
+
+@short.route('/add_link', methods=['POST'])
+def add_link():
+    original_url = request.form['original_url']
+    link = Link(original_url=original_url)
+    db.session.add(link)
+    db.session.commit()
+
+    return render_template('link_added.html',new_link=link.short_url,original_url=link.original_url)
+
+@short.route('/stats')
+def stats():
+    pass
+
+@short.errorhandler(404)
+def page_not_found(e):
+    return '', 404
