273de69af6cc94cc74593b97ec72ac81008ad0f9
nithyarg <nithyaasbsc@gmail.com>
2024-06-06 12:00:57 +0530

    url comp

--- url_shortener/routes.py
+++ url_shortener/routes.py
@@ -3,6 +3,8 @@ from flask import Blueprint,render_template,redirect,request
 from .extensions import db
 from .models import Link
 
+from .auth import requires_auth
+
 from url_shortener.models import Link
 
 short = Blueprint('short', __name__)
@@ -12,14 +14,16 @@ def redirect_to_url(short_url):
     link = Link.query.filter_by(short_url=short_url).first_or_404()
 
     link.visits = link.visits + 1
-    db.session.commits()
+    db.session.commit()
     return redirect(link.original_url)
 
 @short.route('/')
+@requires_auth
 def index():
     return render_template('index.html')
 
 @short.route('/add_link', methods=['POST'])
+@requires_auth
 def add_link():
     original_url = request.form['original_url']
     link = Link(original_url=original_url)
@@ -29,6 +33,7 @@ def add_link():
     return render_template('link_added.html',new_link=link.short_url,original_url=link.original_url)
 
 @short.route('/stats')
+@requires_auth
 def stats():
     links = Link.query.all()
 
@@ -36,4 +41,4 @@ def stats():
 
 @short.errorhandler(404)
 def page_not_found(e):
-    return '<h1>404<h1>', 404
+    return render_template('404.html'), 404
